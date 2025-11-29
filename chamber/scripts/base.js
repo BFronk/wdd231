62bc17f68aaa7898a56f5edca58ea42b6a07c228
Bridger Fronk <bridgerfronk@gmail.com>
2025-11-29 17:31:03 -0500

    updated jooin page

--- chamber/scripts/base.js
+++ chamber/scripts/base.js
@@ -169,3 +169,60 @@ function displayForecastResults(data) {
 // START THE PROCESS
 apiFetch()
 forcecastApiFetch()
+
+//====== JOIN =======
+
+// const bronzeButton = document.querySelector('#learn-bronze')
+// const bronzeModal = document.querySelector('#bronze-modal')
+// const closebtn = document.querySelector('.close-btn')
+
+// bronzeButton.addEventListener("click", () => {
+//   bronzeModal.showModal();
+// });
+
+// closebtn.addEventListener("click", () => {
+//     closebtn.close();
+// })
+
+const nonButton = document.querySelector('#learn-non-profit');
+const nonModal = document.querySelector('#non-profit-modal');
+const nonClose = document.querySelector('#non-close'); 
+const bronzeButton = document.querySelector('#learn-bronze');
+const bronzeModal = document.querySelector('#bronze-modal');
+const bronzeClose = document.querySelector('#bronze-close'); 
+const silverButton = document.querySelector('#learn-silver');
+const silverModal = document.querySelector('#silver-modal');
+const silverClose = document.querySelector('#silver-close');
+const goldButton = document.querySelector('#learn-gold');
+const goldModal = document.querySelector('#gold-modal');
+const goldClose = document.querySelector('#gold-close'); 
+
+nonButton.addEventListener("click", () => {
+    nonModal.showModal();
+});
+
+nonClose.addEventListener("click", () => {
+    nonModal.close();  
+});
+
+bronzeButton.addEventListener("click", () => {
+    bronzeModal.showModal();
+});
+
+bronzeClose.addEventListener("click", () => {
+    bronzeModal.close();  
+});
+silverButton.addEventListener("click", () => {
+    silverModal.showModal();
+});
+
+silverClose.addEventListener("click", () => {
+    silverModal.close();  
+});
+goldButton.addEventListener("click", () => {
+    goldModal.showModal();
+});
+
+goldClose.addEventListener("click", () => {
+    goldModal.close();  
+});
